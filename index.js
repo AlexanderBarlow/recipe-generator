6a048c41a334593b339e5f45bf1344d0bbf18466
alex barlow <alex.barlow@rocketmail.com>
2023-06-17 15:42:38 -0400

    added cards and fixed bugs

---
+++ index.js
@@ -0,0 +1,17 @@
+const show = document.querySelectorAll('.show')
+const hide = document.querySelectorAll('.hide')
+const form = document.querySelector('#container')
+const submitBtn = document.querySelector('#submit')
+
+
+
+submitBtn.addEventListener('onsubmit', Submit) 
+
+function Submit() {
+
+    console.log('button hit');
+
+
+
+}
+
