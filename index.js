a7923c9e500fedd902511139102c50bb4383d7a1
alex barlow <alex.barlow@rocketmail.com>
2023-06-17 16:05:20 -0400

    adding form submit logic

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const show = document.querySelectorAll('.show')
 const hide = document.querySelectorAll('.hide')
 const form = document.querySelector('#container')
 const submitBtn = document.querySelector('#submit')
+const resultForm = document.getElementById('results')
 
 
 
-submitBtn.addEventListener('onsubmit', Submit) 
+form.addEventListener('submit', Submit) 
 
 function Submit() {
 
+    event.preventDefault()
+
     console.log('button hit');
+    console.log(form.classList.contains('show'));
+
+    if (form.classList.contains('show')) {
+
+        console.log('if');
+
+        form.classList.remove('show')
+        form.classList.add('hide')
+        resultForm.classList.remove('hide')
+        resultForm.classList.add('show')
+    }
+
+
 
 
 
