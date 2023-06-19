54fabd51babd19fe17e26879a3dbe74ddba6947a
alex barlow <alex.barlow@rocketmail.com>
2023-06-19 14:03:19 -0400

    added notation

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+
+// VARIABLES NEEDED FOR FORM SUBMISSION AND SHOWING RESULTS
 const show = document.querySelectorAll(".show");
 const hide = document.querySelectorAll(".hide");
 const form = document.querySelector("#container");
 const submitBtn = document.querySelector("#submit");
+
+// VARIABLES NEEDED FOR SEARCH PARAMETERS WITHIN API
+
+
 
 const resultContainer = document.getElementById("resultContainer");
 
@@ -21,4 +27,21 @@ function Submit() {
     resultContainer.classList.remove("hide");
     resultContainer.classList.add("show");
   }
+}
+
+function getRecipes() {
+
+    // FETCH DATA BASED OFF CUISINE INPUT, DIET TYPE AND MACRO SELECTION
+
+    // BREAK DOWN THE FETCHES IF NEEDED AND CROSS REFERENCE RESULTS
+
+    // LIMIT FETCHES TO THREE RESULTS
+
+    // APPEND RESULTS 
+
+
+
+
+
+
 }
