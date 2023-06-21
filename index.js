999b96267865b47d6e409687386ab9372432583a
alex barlow <alex.barlow@rocketmail.com>
2023-06-21 15:27:59 -0400

    retrieved the values from the input form

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hide = document.querySelectorAll(".hide");
 const form = document.querySelector("#container");
 const submitBtn = document.querySelector("#submit");
 
-// VARIABLES NEEDED FOR SEARCH PARAMETERS WITHIN API
+
 
 
 
@@ -14,7 +14,16 @@ const resultContainer = document.getElementById("resultContainer");
 form.addEventListener("submit", Submit);
 
 function Submit() {
+
   event.preventDefault();
+
+
+// VARIABLES NEEDED FOR SEARCH PARAMETERS WITHIN API
+const cuisineInput = document.getElementById('cuisine').value;
+const highProtein = document.getElementById('protein');
+const lowCarb = document.getElementById('carb');
+const lowFat = document.getElementById('fat');
+  const dietInput = document.getElementById('diet').value;
 
   console.log("button hit");
   console.log(form.classList.contains("show"));
@@ -27,9 +36,34 @@ function Submit() {
     resultContainer.classList.remove("hide");
     resultContainer.classList.add("show");
   }
+  
+
+  console.log(dietInput);
+  console.log(cuisineInput);
+
+  if (highProtein.checked == true) {
+    console.log(highProtein.value);
+  }
+  if (lowCarb.checked == true) {
+    console.log(lowCarb.value);
+  }if (lowFat.checked == true) {
+    console.log(lowFat.value);
+  }
+  
 }
 
 function getRecipes() {
+    
+    // fetch(reviewApiTitle).then(function (response) {
+    //     response.json().then(function (data) {
+    //       if (data.results.length === 0) {
+    //         var movieError = document.createElement("p");
+    //         movieError.classList.add("title-text");
+    //         movieError.textContent =
+    //           "Sorry friend!  It looks like we are having trouble finding your       movie!  Please try another movie.";
+    //         movieInfo.append(movieError);
+    //         return;
+    //       }
 
     // FETCH DATA BASED OFF CUISINE INPUT, DIET TYPE AND MACRO SELECTION
 
