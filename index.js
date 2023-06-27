94573f90ce72884867d5ed595a42844b26660a61
alex barlow <alex.barlow@rocketmail.com>
2023-06-27 12:05:35 -0400

    all search parameters function and the returend data is appended to the return form

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const show = document.querySelectorAll(".show");
 const hide = document.querySelectorAll(".hide");
 const form = document.querySelector("#container");
 const submitBtn = document.querySelector("#submit");
-
-
-
-
-
 const resultContainer = document.getElementById("resultContainer");
 
 form.addEventListener("submit", Submit);
@@ -18,37 +13,13 @@ function Submit() {
 
   event.preventDefault();
 
-
-// VARIABLES NEEDED FOR SEARCH PARAMETERS WITHIN API
-const cuisineInput = document.getElementById('cuisine').value;
-const highProtein = document.getElementById('protein');
-const lowCarb = document.getElementById('carb');
-const lowFat = document.getElementById('fat');
-  const dietInput = document.getElementById('diet').value;
-
-  console.log("button hit");
-  console.log(form.classList.contains("show"));
-
   if (form.classList.contains("show")) {
-    console.log("if");
 
     form.classList.remove("show");
     form.classList.add("hide");
     resultContainer.classList.remove("hide");
     resultContainer.classList.add("show");
-  }
-  
 
-  console.log(dietInput);
-  console.log(cuisineInput);
-
-  if (highProtein.checked == true) {
-    console.log(highProtein.value);
-  }
-  if (lowCarb.checked == true) {
-    console.log(lowCarb.value);
-  }if (lowFat.checked == true) {
-    console.log(lowFat.value);
   }
   
 }
@@ -61,12 +32,40 @@ function getRecipes() {
   const highProtein = document.getElementById('protein');
   const lowCarb = document.getElementById('carb');
   const lowFat = document.getElementById('fat');
-  const imgContainer = document.querySelector('#img')
+  const imgContainer = document.querySelector('#img');
+  const tags = document.querySelector('#tags');
+  const dietType = document.querySelector('#diet').value
+  let dietInput
+
+  const newDiet = dietType.split(" ") 
+
+
+if (newDiet.length > 1) {
+  dietInput = ''
+  for (let y = 0; y < newDiet.length; y++) {
+    if (y < newDiet.length - 1) {
+
+     dietInput =  dietInput + newDiet[y] + '-';
+
+    }
+    if (y = newDiet.length - 1 ) {
+      
+      dietInput =  dietInput + newDiet[y];
+
+    }
+
+  }
+} else {
+  dietInput = dietType
+}
+
+console.log(dietInput);
+
 
   const apiId = 'd3e3671d';
   const apiKey = 'df874e873413260268ab781defe839ab';
   const cuisineInput = document.getElementById('cuisine').value;
-  let recipeApi = 'https://api.edamam.com/api/recipes/v2?type=public&q=' + cuisineInput + '&app_id=' + apiId + '&app_key=' + apiKey + '%09&diet='
+  let recipeApi = 'https://api.edamam.com/api/recipes/v2?type=public&q=' + cuisineInput + '&health=' + dietInput.toLowerCase() + '&app_id=' + apiId + '&app_key=' + apiKey + '%09&diet='
 
 //need to conditionally call the macro parameters if they are selected through if statements and different versions of the fetch requested
 
@@ -91,15 +90,19 @@ if ( lowFat.checked === true ) {
 
 }
 
+console.log(recipeApi);
+
 fetch(recipeApi)
 .then(function (response) {
   return response.json();
 })
 .then(function (data) { 
   if (data.length === 0) {
-    alert('Not a valid cuisine!')
+    alert('Sorry, there are no recipes for that.')
     return
   }
+
+  tags.textContent = data.hits[0].recipe.dietLabels
 
   // recipeName.textContent = data.hits[0].recipe.recipeName;
   recipeName.textContent = data.hits[0].recipe.label;
